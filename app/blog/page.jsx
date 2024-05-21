e6b651c1629bd914e529dfecf9b4f390a9656613
adrianputraa <atputrap@gmail.com>
2024-05-21 13:32:20 +0700

    fixes date format, add dynamic metadata

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,10 +1,21 @@
 import DefaultLayout from "@/components/layout/default-layout"
 import IndexPage from "@/components/blog/index"
+import { recentPost } from "@/lib/constant/recent-post"
 
-export default function BlogIndex() {
+export default function BlogIndex({ searchParams }) {
+    const { cat } = searchParams
+
+    const availablePost = () => {
+        if (cat) {
+            return recentPost.filter((item) => item.tags[0] === cat)
+        }
+
+        return recentPost
+    }
+
     return (
         <DefaultLayout>
-            <IndexPage />
+            <IndexPage list={availablePost()} />
         </DefaultLayout>
     )
 }
--- app/blog/post/[slug]/page.jsx
+++ app/blog/post/[slug]/page.jsx
@@ -2,13 +2,33 @@ import DefaultLayout from "@/components/layout/default-layout";
 import Post from '@/components/blog/post'
 import { recentPost } from '@/lib/constant/recent-post'
 
+export async function generateMetadata({ params }) {
+    const { slug } = params
+    const post = recentPost.filter((item) => item.slug === slug)[0]
+    const titlePreview = post.title
+    const contentPreview = post.article.substring(0, 20)
+    const imagePreview = '/images/profileadrianputra-_400x400.jpg'
+
+    const metadata = {
+      title: `${titlePreview} - adrianputraa`,
+      openGraph: {
+        description: `${contentPreview}`,
+        images: imagePreview
+      }
+    }
+
+    return metadata
+}
+
 export default function BlogPost({ params }) {
   const { slug } = params
   const post = recentPost.filter((item) => item.slug === slug)[0]
 
   return (
-    <DefaultLayout>
-      <Post blog={post} />
-    </DefaultLayout>
+    <>
+      <DefaultLayout>
+        <Post blog={post} />
+      </DefaultLayout>
+    </>
   );
 }
